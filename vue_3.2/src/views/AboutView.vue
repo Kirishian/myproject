05bf66ec8006c12653a1e5d3147f6d216303282b
liutao <1710959781@qq.com>
2023-12-29 00:15:14 +0800

    master

--- vue_3.2/src/views/AboutView.vue
+++ vue_3.2/src/views/AboutView.vue
@@ -5,5 +5,7 @@
     <h1>This is an about page</h1>
     <h1>This is an about page</h1>
     <h1>This is an about page</h1>
+    <h1>This is an about page</h1>
+    <h1>This is an about page</h1>
   </div>
 </template>
